37110e603dea5402c30c42423461fd8ab60f84f4
Niels <niels.rogge1@gmail.com>
2023-02-28 12:36:30 +0100

    Add new version

--- coco_eval/__init__.py
+++ coco_eval/__init__.py
@@ -1,4 +1,3 @@
 # The __init__.py file is used to mark which classes you want the user to access through the package interface.
 # from packagename.Filename import Classname
 from coco_eval.coco_eval import CocoEvaluator
-from coco_eval.utils import all_gather
--- setup.py
+++ setup.py
@@ -1,8 +1,9 @@
-import setuptools
+from setuptools import setup, find_packages
 
-setuptools.setup(
+setup(
     name="coco_eval",
-    version="0.0.2",
+    version="0.0.3",
+    packages = find_packages(exclude=[]),
     author="Niels Rogge",
     author_email="niels.rogge1@gmail.com",
     description="A tiny package containing a COCO evaluator that works in distributed environments.",
